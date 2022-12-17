ee90aea102a93b69597b2f1f972d1339a3663c61
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2022-12-17 19:49:40 +0530

    C Program to Check Whether a Number is Even or Odd

---
+++ ternaryOperator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+  int num;
+  printf("enter a number : ");
+  scanf("%d",&num);
+  
+  num % 2 == 0 ? printf("%d is even number\n",num) : printf ("%d is odd number\n",num);
+  return 0;
+}
