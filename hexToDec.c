f43920cc852a9ef8d439654466ecb972fa589899
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2022-12-19 21:34:20 +0530

    C program to demonstrate hexadecimal to decimal

---
+++ hexToDec.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int n;
+	// taking input from user
+	scanf("%x", &n);
+	
+	// printing the decimal number
+	printf("%d\n", n);
+	return 0;
+}
+
+// conversion using format specifier
