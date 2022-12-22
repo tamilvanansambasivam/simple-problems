0d502e58b5fab1f67b5cdffa86278fc85ba4302b
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2022-12-22 21:07:47 +0530

    Sum of Digits of a Five Digit Number

---
+++ sumOfDigits.c
@@ -0,0 +1,21 @@
+//Sum of Digits of a Five Digit Number (hackerrank solution)
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n;
+    scanf("%d", &n);
+    //Complete the code to calculate the sum of the five digits on n.
+    int sum;
+    while(n!=0)
+    {
+        sum=sum+(n%10);
+        n = n/10;
+    }
+
+    printf("%d", sum);
+    return 0;
+}
